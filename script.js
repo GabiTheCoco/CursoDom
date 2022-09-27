cdd16365c0ad7791f2bc97860312c1a159a62e8b
Gabriel <gabiabaldee@gmail.com>
2022-09-27 17:16:33 -0300

    final clase03

--- script.js
+++ script.js
@@ -9,7 +9,26 @@ const btnAgregar = document.querySelector("[data-form-btn]");
 const createTask = (evento) => { 
     evento.preventDefault(); // !previene el comportamiento por defecto del formulario, o un objeto en específico (recargar la pagina en este caso)
     const inputText = document.querySelector("[data-form-input]");
-    console.log(inputText.value);
+    const value = inputText.value;
+    inputText.value = "";
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+
+    const content = ` <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  /*
+    * esto se llama string template, es una propieda de JS que permite
+    * guardar un elmento html y poder utilizarlo con variables de JS
+    * como se muestra en la línea 18
+  */
+
+    task.innerHTML = content; // * lo q esta adentro d task cambia a content.
+    list.appendChild(task);
+    console.log(content);
 }
 
 
